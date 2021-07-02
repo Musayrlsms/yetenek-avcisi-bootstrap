c2be821f969f7d7f6fe7aa41bedd6b0f85e1cc34
Musa Yerleşmiş <musa.yerlesmis.tr@gmail.com>
2021-07-02 23:43:27 +0300

    routes pathleri düzenlendi

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,18 @@ Rails.application.routes.draw do
   
   
   root to: 'homes#index'
-  devise_for :users, controllers: { sessions: 'em/users/sessions', registrations: 'em/users/registrations', passwords: 'em/users/passwords'}
+  
   namespace :wo do
+    devise_for :users, controllers: { sessions: 'wo/users/sessions', registrations: 'wo/users/registrations', passwords: 'wo/users/passwords'}
     root to: 'workers#index', as: "workers"
     resources :workers
   end
   namespace :em do
+    devise_for :users, controllers: { sessions: 'em/users/sessions', registrations: 'em/users/registrations', passwords: 'em/users/passwords'}
     root to: 'employers#index',as: "employers"
     resources :employers
     resources :companies
+    get '/employers/show' => "employer#index", :as => 'user_root'
     
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
